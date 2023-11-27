ca3265deecb4ed18f59e13410ede0ff6ee178c92
LeonGitonga <leon.gitonga@student.moringaschool.com>
2023-11-27 19:51:19 +0300

    Lab done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,12 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 0:
+        return ""
+    elif len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    else:
+        comma_separation = ", ".join(items[:-1])
+        return f"{comma_separation}, and {items[-1]}"
+
+
